82fcb986b9231a2b180f76d5234c42a2b96c5e11
Ethan Muller <hi@emu.media>
2022-05-10 01:55:14 -0400

    fix path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteFaviconsPlugin()]
+  plugins: [vue(), ViteFaviconsPlugin('./src/assets/logo.png')]
 })
